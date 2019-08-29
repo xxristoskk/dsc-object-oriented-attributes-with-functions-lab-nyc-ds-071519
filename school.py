9e61402438cd9d02a3b68f8cbe9d7f299040623e
Xristos <xristos.kvtsvros@gmail.com>
2019-08-29 17:35:55 -0400

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,8 @@
-class School:
+class School():
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+        pass
+    def add_student(self,name,grade):
+        self.roster[grade] = [name]
+        pass
